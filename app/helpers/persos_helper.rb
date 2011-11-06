33d636ad0fe3c5daba7c7233db20ecbba9817258
Kamiben <paul_atreide_dune@hotmail.com>
2011-11-06 19:15:19 +0100

    Working on presentation (carac totale)

--- app/helpers/persos_helper.rb
+++ app/helpers/persos_helper.rb
@@ -7,8 +7,37 @@ end
 def tabcarac(attribut)
   out = ""
 attribut.each do |a| 
-    out = out << "<td>#{a}</td> <td> #{@perso.attributs.find_by_nom(a).val}</td>"
+    if (@perso.attributs.find_by_nom(a).nil?)
+      out = out << "<td>#{a}</td> <td> 0</td>"
+    else 
+      out = out << "<td>#{a}</td> <td> #{@perso.attributs.find_by_nom(a).val}</td>"
+  end
   end
 out
+end
+
+def tabcaractotale(attribut)
+  out = "<tr>"
+  trice = 0
+  attribut.each do |a|     
+    trice = trice +1
+    if (@perso.attributs.find_by_nom(a).nil?)
+      if (trice==3)
+       out = out << "<td>#{a}</td> <td> 0</td></tr>"
+       trice = 0
+      else 
+        out = out << "<td>#{a}</td> <td> 0</td>"
+      end
+    else 
+      if (trice==3)
+       out = out << "<td>#{a}</td> <td> #{@perso.attributs.find_by_nom(a).val}</td></tr>"
+       trice = 0
+      else 
+        out = out << "<td>#{a}</td> <td> #{@perso.attributs.find_by_nom(a).val}</td>"
+      end
+    end
+  
+  end
+  out
 end
 end
